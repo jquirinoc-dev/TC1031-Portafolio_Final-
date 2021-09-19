b2c54f98cb20842158a9a4ac067ef2f2752055eb
Jonathan Quirino <75098397+jonathan619@users.noreply.github.com>
2021-09-19 15:29:21 -0500

    Cambios (Estandar de codificacion)

--- Act1.3/main.cpp
+++ Act1.3/main.cpp
@@ -1,20 +1,34 @@
+/* La funcion de este programa es leer un archivo de texto en el que tenemos registros con fechas y con el algoritmo de ordenamiento QuickSort, ordenamos un arreglo
+y desplegamos en pantalla un rango de registros en base a dos fechas delimitantes introducidas por el usuario. 
+
+Jonathan Joaquín Quirino Carrasco | A01640100
+
+Fecha de creación: Jueves 16 de Septiembre 2021
+Fecha de modificación: Domingo 19 de Septiembre 2021
+
+*/
+
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include "dateTime.h"
 
+
+/* Una estructura la cual contendrá un objeto fecha, un string de fecha y un string de ip + descripcion */
 struct LOG{
     dateTime date;
     std::string dateString;
     std::string ipDesc;
 } logs;
 
+/* En esta funcion se intercambian las posiciones de dos valores tipo fecha en, este caso, un vector */
 void swap(LOG* a, LOG* b){
     LOG t = *a;
     *a = *b;
     *b = t;
 }
- 
+
+/* Se crea una particion en nuestro vector para empezar iterar a partir de ella, se nos regresa i + 1 */
 int partition (std::vector<LOG> &arr, int low, int high){
     
     dateTime pivot = arr[high].date;
@@ -32,7 +46,8 @@ int partition (std::vector<LOG> &arr, int low, int high){
     swap(&arr[i + 1], &arr[high]);
     return (i + 1);
 }
- 
+
+/* Con esta funcion ordenamos nuestro vector, toma como parametros el vector, la posicion inicial y la final */ 
 void quickSort(std::vector<LOG> &arr, int low, int high){
     if (low < high){
 
@@ -43,15 +58,16 @@ void quickSort(std::vector<LOG> &arr, int low, int high){
     }
 }
 
-int binarySearch(std::vector<LOG> vectorLogs, int n, dateTime k, int *compara) {
+/* En este funcion recibimos nuestro vector vectorLogs, el tamaño n del mismo y el valor a buscar k 
+Obtenemos la posición donde se encuentra k. De no estar, se nos regresa -1*/
+int binarySearch(std::vector<LOG> vectorLogs, int n, dateTime k) {
   int low = 0;
   int high = n - 1;
   
-  *compara = 0;
 
   while (low <= high) {
     int m = low + (high - low) / 2;
-    (*compara)++;
+
     if (k == vectorLogs[m].date){
         return m;
     }else if (k < vectorLogs[m].date) {
@@ -64,6 +80,9 @@ int binarySearch(std::vector<LOG> vectorLogs, int n, dateTime k, int *compara) {
   return -1;
 }
 
+/* Recibimos nuestro vector y una variable int igual a cero la cual servirá para calcular el tamaño de nuestro vector
+En esta funcion leemos el archivo bitacora.txt y, por cada linea, creamos un struct tipo LOG que guarda los datos
+para manipular y ordenar nuestro vector mas adelante */
 void loadLogs(std::vector<LOG> &vectorLogs, int &logNums){
     logNums = 0;
     std::string month = "";
@@ -101,6 +120,7 @@ void loadLogs(std::vector<LOG> &vectorLogs, int &logNums){
     }
 }
 
+/* Por cada posicion del vector generamos un string que iremos escribiendo linea por linea en un archivo nuevo llamado bitacora_ordenada.txt */
 void writeToNewTxt(std::vector<LOG> vectorLogs, int numLogs){
     
     std::ofstream out("bitacora_ordenada.txt");
@@ -121,6 +141,8 @@ void writeToNewTxt(std::vector<LOG> vectorLogs, int numLogs){
     out.close();
 }
 
+/* Imprimimos el rango de valores en nuestro vectordependiendo de las posiciones (x, y) que hemos obtenido de nuestra binarySearch, 
+donde n es el tamaño del vector */
 void printRange(std::vector<LOG> vectorLogs, int n, int x, int y){
 
     for (int i = x; i <= y; i++){
@@ -129,6 +151,8 @@ void printRange(std::vector<LOG> vectorLogs, int n, int x, int y){
     }
 }
 
+
+/* Funcion principal del programa */
 int main(){
 
     std::vector<LOG> vectorLogs;
@@ -139,12 +163,6 @@ int main(){
     loadLogs(vectorLogs, n);
     quickSort(vectorLogs, 0, n - 1);
     
-
-    /* for (int i = 0; i < n; i++){
-        std::cout << vectorLogs[i].dateString << " ";
-        std::cout << vectorLogs[i].ipDesc << std::endl;
-    } */
-
     std::cout << "El arreglo ha sido ordenado!!" << std::endl;
 
     writeToNewTxt(vectorLogs, n);
@@ -159,12 +177,9 @@ int main(){
 
     std::cout << "Fecha de inicio:" << std::endl;
     std::getline(std::cin, val1);
-    /* std::cout << val1 << std::endl; */
+
     std::cout << "Fecha de fin:" << std::endl;
     std::getline(std::cin, val2);
-    /* std::cout << val2 << std::endl; */
-
-    int comparaciones = 0;
 
     std::string month = val1.substr(0,3);
     int day = stoi(val1.substr(4,6));
@@ -174,7 +189,7 @@ int main(){
 
     dateTime fecha1(month, day, hour, min, sec);
 
-    res1 =  binarySearch(vectorLogs, n, fecha1, &comparaciones);
+    res1 =  binarySearch(vectorLogs, n, fecha1);
 
     month = val2.substr(0,3);
     day = stoi(val2.substr(4,6));
@@ -183,19 +198,7 @@ int main(){
     sec = stoi(val2.substr(13,15));
 
     dateTime fecha2(month, day, hour, min, sec);
-    res2 =  binarySearch(vectorLogs, n, fecha2, &comparaciones);
-
-    /* if (res1 == -1){
-        std::cout << "Element not found" << std::endl;
-    } else{
-        std::cout << "Element date1 found in pos " << res1 << std::endl;
-    }
-
-    if (res2 == -1){
-        std::cout << "Element not found" << std::endl;
-    } else{
-        std::cout << "Element date2 found in pos " << res2 << std::endl;
-    } */
+    res2 =  binarySearch(vectorLogs, n, fecha2);
 
     if (res1 == -1 || res2 == -1){
         std::cout << "Las fechas o alguna de las fechas introducidas no han sido encontradas en los registros.\nIntenta correr de nuevo el programa e introducir fechas diferentes" << std::endl;
