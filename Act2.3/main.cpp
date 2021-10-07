0c7e99b38ce04b18f677ddab09a3550e8897a1e2
Jonathan Quirino <75098397+jonathan619@users.noreply.github.com>
2021-10-07 12:54:30 -0500

    Main, asesoria

--- Act2.3/main.cpp
+++ Act2.3/main.cpp
@@ -1,53 +1,8 @@
-#include <iostream>
-#include <fstream>
-#include "dateTime.h"
-#include "DoubleLinkedList.h"
 #include "Log.h"
+#include "DoubleLinkedList.h"
 
-struct LOG{
-    dateTime date;
-    std::string dateString;
-    std::string ipDesc;
-} logs;
 
-dateTime generateDate(std::string stringDate){
-
-    std::string month = stringDate.substr(0,3);
-    int day = stoi(stringDate.substr(4,6));
-    int hour = stoi(stringDate.substr(7,9));
-    int min = stoi(stringDate.substr(10,12));
-    int sec = stoi(stringDate.substr(13,15));
-    
-    dateTime date(month, day, hour, min, sec);
-
-    return date;
-}
-
-void loadLogs(DoubleLinkedList<Log> &logList, int &logNums){
-    logNums = 0;
-/*     std::string month = "";
-    std::string day = "";
-    std::string time = ""; */
-
-    std::ifstream file;
-
-    file.open("bitacora.txt");
-
-    std::string line;
-
-    while (getline(file, line)){
-
-        dateTime date = generateDate(line);
-        std::string fullDate = line.substr(0,15);
-        std::string ipDesc = line.substr(16, line.size());
-
-        Log newLog(date, fullDate, ipDesc);
-        logList.addLast(newLog);
-
-        logNums++;
-    }
-}
-
+/*
 void writeToNewTxt(DoubleLinkedList<Log> logList, int start, int end, std::string fileName){
     
     std::ofstream out(fileName);
@@ -74,6 +29,7 @@ void printLogs(DoubleLinkedList<Log> logList, int n){
         std::cout << logList.getData(i).getStringDate() << " " << logList.getData(i).getIpDesc() << std::endl;
     }
 }
+*/
 
 int main(){
 
@@ -81,13 +37,13 @@ int main(){
     DoubleLinkedList<Log> myLogList;
     int n = 0;
 
-    loadLogs(myLogList, n);
-    /* myLogList.printList(); */
+    myLogList.loadLogs();
+
     
 
     /* printLogs(myLogList, n); */
-    myLogList.sort();
-    printLogs(myLogList, n);
+    //myLogList.sort();
+    //printLogs(myLogList, n);
     /* writeToNewTxt(myLogList, 0, n, "bitacora ordenada.txt"); */
     
 
